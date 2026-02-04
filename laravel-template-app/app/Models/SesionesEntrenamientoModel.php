8e32a73269628133ec475c7e6a4ded2884481780
ikermrtnzq <iker.martin44@educa.madrid.org>
2026-02-04 13:14:39 +0000

    Modificado modelo de sesiones de entramiento

--- laravel-template-app/app/Models/SesionesEntrenamientoModel.php
+++ laravel-template-app/app/Models/SesionesEntrenamientoModel.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class SesionesEntrenamientoModel extends Model
 {
-    //
+    protected $table = 'sesion_entrenamiento';
+
+    protected $fillable = [
+        'id_plan',
+        'fecha',
+        'nombre',
+        'descripcion',
+        'completada'
+    ];
+
+    public $timestamps = false;
 }
