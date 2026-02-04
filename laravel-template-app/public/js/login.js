2e03ce38d68ff46302a6f22e61b4f6621ca79d24
ibriiii <ibrahimcollado8@gmail.com>
2026-02-04 16:57:58 +0100

    Implementar alternancia entre formularios de login y registro
    - Añadir JavaScript para mostrar/ocultar formularios dinámicamente
    - Actualizar HTML con IDs y atributo defer en script

--- laravel-template-app/public/js/login.js
+++ laravel-template-app/public/js/login.js
@@ -1,1 +1,13 @@
-console.log(window.location.origin);
+const links = document.querySelectorAll("#show-register, #show-login");
+
+links.forEach(link => link.addEventListener("click", mostrarRegistro_mostrarInicio))
+
+
+function mostrarRegistro_mostrarInicio(e) {
+	const registro = document.getElementById("registro-container");
+	const inicio = document.getElementById("inicio-container");
+
+	inicio.classList.toggle("hidden");
+	registro.classList.toggle("hidden");
+}
+
