3be3c89cf389a67a4832cfd4d7e77d7feafbc6ac
ikermrtnzq <iker.martin44@educa.madrid.org>
2026-02-05 07:45:14 +0000

    Creada validacion de bloques entrenamiento bbdd

--- laravel-template-app/app/Http/Controllers/BloquesEntrenamientoController.php
+++ laravel-template-app/app/Http/Controllers/BloquesEntrenamientoController.php
@@ -18,7 +18,18 @@ class BloquesEntrenamientoController extends Controller
 
     public function crearBloque(Request $request)
     {
-        $bloque = BloqueEntrenamiento::create($request->all());
+        $datos = $request->validate([
+            'nombre' => 'required|string|max:255',
+            'descripcion' => 'nullable|string',
+            'tipo' => 'required|string|in:rodaje,intervalos,recuperacion',
+            'duracion_estimada' => 'required|date_format:H:i:s',
+            'potencia_pct_min' => 'required|integer|min:0|max:100',
+            'potencia_pct_max' => 'required|integer|min:0|max:100|gte:potencia_pct_min',
+            'pulso_pct_max' => 'nullable|integer|min:0|max:100',
+            'pulso_reserva_pct' => 'nullable|integer|min:0|max:100',
+            'comentario' => 'nullable|string'
+        ]);
+        $bloque = BloqueEntrenamiento::create($datos);
 
         return response()->json([
             'status' => 'ok',
