21485a9da0405153ebc9c0b0d3c4dd0b82a49ac4
Ibrahim <ibrahim@localhost>
2026-02-09 13:50:47 +0100

    Arreglo de javascript para la tabla plan y mejora de diseño para la tabla. Modularizacion del codigo para su correcta escalabilidad.

--- laravel-template-app/public/js/index/plan.js
+++ laravel-template-app/public/js/index/plan.js
@@ -5,23 +5,31 @@ const datos = [];
 /* ESCUCHADORES */
 enlacePlan.addEventListener("click", obtenerPlan);
 
-/* FUNCIONES */
+//FUNCIONES PARA TAREAS ESPECIFICAS
+
+function isEmpty(obj) {return !obj || obj.length === 0;}
+
+/* FUNCIONES CORRIENTES */
 
 /* OBTENCION DE DATOS */
 async function obtenerPlan() {
 	try {
+		//EXTRAEMOS LOS DATOS DE LA TABLA 
 		const response = await fetch("/plan", {
+			//METODO QUE USAMOS
 			method: "GET",
-			signal: AbortSignal.timeout(20000)
+			//TIEMPO QUE VAMOS A ESPERAR LA RESPUESTA
+			signal: AbortSignal.timeout(5000)
 		});
-
-		datos.push(await response.json());
-
+		
+		//COMPROBAMOS QUE LA EXTRACCION A SALIDO BIEN
 		if (!response.ok) {
 			throw new Error("Error al obtener planes...");
 		} else {
-			console.log(datos);
-			if (datos) mostrarDatos();
+			//AÑADIMOS LOS DATOS AL ARRAY GLOBAL CREADO PARA LOS DATOS
+			datos.push(await response.json());
+			// SI NO HUBIERA DATOS NO MOSTRAMOS NADA
+			if (!isEmpty(datos)) mostrarDatos();
 		}
 	} catch(error) {
 		console.log(error);
@@ -41,31 +49,54 @@ function obtenerTitulos() {
 
 function mostrarDatos() {
 	/* VARIABLES */
-	const titulos = obtenerTitulos();
 	const contenedor = document.getElementById("main");
-	const arrayDatos = datos[0].planes;
-
-	/* DECLARACIONES DE ELEMENTOS DEL DOM */
 	const table = document.createElement("table");
+	
+	//INVOCAMOS FUNCIONES QUE CREAR LAS PARTES DE LA TABLA E INSERTAMOS EN LA MISMA
+	table.append(creacionCabecera(), creacionCuerpo());
+	contenedor.append(table);
+}
+
+// CREACION DE LA CABECERA CON LOS TITULOS DINAMICOS DE LAS TABLAS
+function creacionCabecera() {
+	//VARIABLES
+	const titulos = obtenerTitulos();
 	const thead = document.createElement("thead");
-	const tbody = document.createElement("tbody");
 	const trHead = document.createElement("tr");
 
-	/* CREACION DE CABECERA */
+	// ITERAMOS SOBRE EL ARRAY DE TITULOS
 	for(let nombre in titulos) {
 		const th = document.createElement("th");
 		th.textContent = titulos[nombre];
 		trHead.append(th);
 	}
 	thead.append(trHead);
-	console.log(arrayDatos[0])
-	for (let i = 0; i < arrayDatos.length; i++) {
-		const tr = document.createElement("tr");
-		
-		for(let a = 0; a < titulos.length; a++) {
-			/* console.log(arrayDatos[i].titulos[a]); */
-		}
-	}
+	return thead;
+}
 
-	table.append(tbody);
+// CREACION DE EL CUERPO DE LAS TABLAS
+function creacionCuerpo() {
+	//VARIABLES
+	const tbody = document.createElement("tbody");
+	const arrayDatos = datos[0].planes;
+
+	// ACCEDEMOS AL ARRAY DE OBJETOS
+	arrayDatos.forEach(objeto => {
+        const tr = document.createElement("tr");
+
+        // EXTRAEMOS LOS VALORES DE CADA UNO DE LOS OBJETOS
+        const valores = Object.values(objeto);
+		console.log(valores);
+
+		//ITERAMOS SOBRE LOS VALORES EXTRAIDOS PARA INCORPORAR LAS FILAS Y COLUMNAS
+        valores.forEach(valor => {
+            const td = document.createElement("td");
+            td.textContent = valor;
+            tr.append(td);
+        });
+
+        tbody.append(tr);
+    });
+
+	return tbody;
 }
