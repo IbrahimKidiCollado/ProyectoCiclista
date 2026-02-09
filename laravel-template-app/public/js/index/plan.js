dfab3889493e7c4412561b614eb00a70ad3a391d
ibriiii <ibrahimcollado8@gmail.com>
2026-02-09 10:16:44 +0100

    Muestreo de thead vaciado del main provisional y cambio de estilos para tabla

--- laravel-template-app/public/js/index/plan.js
+++ laravel-template-app/public/js/index/plan.js
@@ -10,23 +10,54 @@ enlacePlan.addEventListener("click", obtenerPlan);
 /* OBTENCION DE DATOS */
 async function obtenerPlan() {
 	try {
-		console.log("hola");
 		const response = await fetch("/plan", {
 			method: "GET",
-			signal: AbortSignal.timeout(25000)
+			signal: AbortSignal.timeout(20000)
 		});
 
-		if (!response.ok) throw new Error("Error al obtener planes...");
-
 		datos.push(await response.json());
+
+		if (!response.ok) {
+			throw new Error("Error al obtener planes...");
+		} else {
+			console.log(datos);
+			if (datos) mostrarDatos();
+		}
 	} catch(error) {
 		console.log(error);
 	}
-
-	if (datos) mostrarDatos();
 }
 
 /* TRATAMIENTO DE DATOS */
+function obtenerTitulos() {
+	const titulos = [];
+
+	let nombres = datos[0].planes;
+
+	Object.keys(nombres[0]).forEach(d => titulos.push(d));
+
+	return titulos;
+}
+
 function mostrarDatos() {
-	console.log(datos);
+	/* VARIABLES */
+	const titulos = obtenerTitulos();
+	const contenedor = document.getElementById("main-content");
+
+	/* DECLARACIONES DE ELEMENTOS DEL DOM */
+	const table = document.createElement("table");
+	const thead = document.createElement("thead");
+	const tbody = document.createElement("tbody");
+	const tr = document.createElement("tr");
+
+	/* CREACION DE CABECERA */
+	for(let nombre in titulos) {
+		const th = document.createElement("th");
+		th.textContent = titulos[nombre];
+		thead.append(th);
+	}
+
+	table.append("thead");
+
+	contenedor.append(table);
 }
